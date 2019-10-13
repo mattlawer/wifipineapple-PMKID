5481325119bebd5086abcf01f615e547d491062a
Mathieu <mathieu@prototyper.fr>
2019-10-13 19:55:27 +0200

    updated for firmware 2.6.1

--- PMKID/scripts/dependencies.sh
+++ PMKID/scripts/dependencies.sh
@@ -12,16 +12,14 @@ touch /tmp/PMKID.progress
 
 if [ "$1" = "install" ]; then
   # Let's always get the latest version
-  mkdir -p /tmp/HcxTools
-  wget https://github.com/adde88/hcxtools-hcxdumptool-openwrt/tree/master/bin/ar71xx/packages/base -P /tmp/HcxTools
-  HCXDUMPTOOL=`grep -F "hcxdumptool_" /tmp/HcxTools/base | awk {'print $5'} | awk -F'"' {'print $2'} | grep "ar71xx" `
-  HCXTOOLS=`grep -F "hcxtools_" /tmp/HcxTools/base | awk {'print $5'} | awk -F'"' {'print $2'} | grep "ar71xx"`
+  HCXDUMPTOOL="hcxdumptool-custom_5.2.2-2_mips_24kc.ipk"
+  HCXTOOLS="hcxtools-custom_5.2.2-2_mips_24kc.ipk"
 
   # Download latest IPK's to temp directory, and then update OPKG repositories.
   cd /tmp
   opkg update
-  wget "https://github.com/adde88/hcxtools-hcxdumptool-openwrt/raw/master/bin/ar71xx/packages/base/"$HCXTOOLS""
-  wget "https://github.com/adde88/hcxtools-hcxdumptool-openwrt/raw/master/bin/ar71xx/packages/base/"$HCXDUMPTOOL""
+  wget "https://raw.githubusercontent.com/adde88/hcxtools-hcxdumptool-openwrt/openwrt-19.07/bin/packages/mips_24kc/custom/"$HCXTOOLS""
+  wget "https://raw.githubusercontent.com/adde88/hcxtools-hcxdumptool-openwrt/openwrt-19.07/bin/packages/mips_24kc/custom/"$HCXDUMPTOOL""
 
   if [ "$2" = "internal" ]; then
     # Tetra install / general install.
@@ -40,7 +38,7 @@ if [ "$1" = "install" ]; then
   uci commit PMKID.module.installed
 
   # Cleanup
-  rm -rf "$HCXTOOLS" "$HCXDUMPTOOL" /tmp/HcxTools/
+  rm -rf "$HCXTOOLS" "$HCXDUMPTOOL"
   echo -e "${RED}Installation completed!"
 
 elif [ "$1" = "remove" ]; then
